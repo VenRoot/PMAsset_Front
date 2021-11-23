d2cc6454884a61851f7af973124850771fae9a11
Ven <0ventox0@gmail.com>
2021-11-23 16:33:43 +0100

    Values für die Interfaces

---
+++ src/js/values.ts
@@ -0,0 +1,59 @@
+import { DevTypes, phoneTypes, Bildschirm, MonHersteller, Status } from "./interface";
+
+export const PCTypen: DevTypes[] = Object.seal([
+    "CAD",
+    "T14",
+    "T30",
+    "T50",
+    "T70",
+    "T80",
+    "T90",
+    "Tower"
+]);
+
+export const StatusTypen: Status[] = Object.seal([
+    "Aktiv", 
+    "Inaktiv", 
+    "Bestellt", 
+    "Defekt", 
+    "Reserviert", 
+    "Verschrottet"
+]);
+
+export const PhoneTypen: phoneTypes[] = Object.seal([
+    "iPhone 6",
+    "iPhone 6S",
+    "iPhone 6 Plus",
+    "iPhone 6S Plus",
+    "iPhone SE",
+    "iPhone 7",
+    "iPhone 7 Plus",
+    "iPhone 7S",
+    "iPhone 7S Plus",
+    "iPhone 8",
+    "iPhone 8 Plus",
+    "iPhone X",
+    "iPhone XS",
+    "iPhone XS Max",
+    "iPhone XR",
+    "iPhone 11",
+    "iPhone 11 Pro",
+    "iPhone 11 Pro Max",
+    "iPhone SE 2. Gen",
+    "iPhone 12",
+    "iPhone 13",
+    "iPhone 13 Mini",
+    "iPhone 13 Pro",
+    "iPhone 13 Pro Max"
+]);
+
+export const MonitorHersteller: MonHersteller[] = Object.seal([
+    "Samsung",
+    "LG",
+    "Dell"
+]);
+
+
+export const MonitorTypen: Bildschirm[] = Object.seal([
+
+]);
