c7c1e0cd64229b573448dad5ce4e93071ecfc908
VenRoot <0ventox0@gmail.com>
2022-03-08 08:41:04 +0100

    Changed port

--- src/js/vars.ts
+++ src/js/vars.ts
@@ -1,2 +1,2 @@
 export const SERVERADDR = navigator.appVersion.indexOf("Win")!=-1 ? "https://azrweunodejs01:5000/" : "https://localhost:5000/";
-export const FRONTADDR = navigator.appVersion.indexOf("Win")!=-1 ? "https://azrweunodejs01:3000/" : "https://localhost:3000/";
+export const FRONTADDR = navigator.appVersion.indexOf("Win")!=-1 ? "https://azrweunodejs01:4000/" : "https://localhost:4000/";
